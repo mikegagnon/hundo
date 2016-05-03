3f9fc576b10dac555fcede9bf595e56bfede9d67
Michael Gagnon <mikegagnon@gmail.com>
2016-05-02 20:44:40 -0600

    drawing data

---
+++ public/js/hundo.js
@@ -0,0 +1,43 @@
+
+var viz;
+var board;
+
+var vizConfig = {
+    cellSize : 32
+}
+
+board = {
+    num_rows: 5,
+    num_cols: 10,
+    blocks : [
+        {
+            row: 2,
+            col: 1
+        },
+        {
+            row: 2,
+            col: 2
+        }
+    ]
+}
+
+viz = d3.select("#boardSvg")
+    .attr("width", board.num_cols * vizConfig.cellSize)
+    .attr("height", board.num_rows * vizConfig.cellSize);
+
+viz.select("#background")
+    .attr("width", board.num_cols * vizConfig.cellSize)
+    .attr("height", board.num_rows * vizConfig.cellSize)
+    .attr("style", "fill:black");
+
+viz.selectAll(".block")
+    .data(board.blocks)
+    .enter()
+    .append("svg:use")
+    .attr("class", "block")
+    .attr("xlink:href", "#blockTemplate")
+    .attr("transform", function(block) {
+      var x = block.col * vizConfig.cellSize;
+      var y = block.row * vizConfig.cellSize;
+      return "translate(" + x + ", " + y + ") "
+    })
